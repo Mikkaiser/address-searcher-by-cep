2ad4de78845b4adbd44d0f51f2dc626e4cc13f0e
Mikael Ribeiro Simões <mikaelrsimoes19@gmail.com>
2021-07-14 00:03:36 -0300

    feat: add component configurations

--- src/app/modules/search/components/fields/fields.component.ts
+++ src/app/modules/search/components/fields/fields.component.ts
@@ -4,19 +4,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-fields',
   templateUrl: './fields.component.html',
-  styleUrls: ['./fields.component.scss']
+  styleUrls: ['./fields.component.scss'],
 })
 export class FieldsComponent implements OnInit {
+  constructor(private fieldsService: FieldsService) {}
 
-  constructor(private fieldsService: FieldsService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
-  }
-
-  getData(value: any,  form: any) : any {
+  getData(value: any): any {
     //this function will be created later
-    this.fieldsService.getData(value).subscribe((data: any) => this.insertOnFields(data, form));
+    this.fieldsService
+      .getData(value)
+      .subscribe((data: any) => this.insertOnFields(data, form));
   }
 
+  insertOnFields(data: any, form: any) {
+   
+  }
 }
